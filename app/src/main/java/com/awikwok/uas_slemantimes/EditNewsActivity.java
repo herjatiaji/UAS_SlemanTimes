57a289e4f7b5d46a7182daea143fcdeaa294d462
herjatiaji <herjati.aji@mail.ugm.ac.id>
2022-12-24 08:06:49 +0700

    initial commit

---
+++ app/src/main/java/com/awikwok/uas_slemantimes/EditNewsActivity.java
@@ -0,0 +1,14 @@
+package com.awikwok.uas_slemantimes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_news);
+    }
+}
